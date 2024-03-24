7f75bb27934a2e8bfce9d68cd8a1cb3cd5c87e48
pawaderahul <rahulpawade003@gmail.com>
2024-03-24 05:42:28 +0530

    Ft: Our Class Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import Home from "./pages/Home/Home";
 import Nav from "./Layout/Nav/Nav";
-import WhyChooseUse from "./pages/Why-Choose-Us/WhyChooseUse";
+import WhyChooseUs from "./pages/Why-Choose-Us/WhyChooseUs";
 import AboutUs from "./pages/About-Us/AboutUs";
 import MoveToTop from "./shared-components/Move-To-Top/MoveToTop";
+import OurClass from "./pages/Our-Class/OurClass";
 
 const App = () => {
   return (
     <>
       <Nav />
       <Home />
-      <WhyChooseUse />
+      <WhyChooseUs />
       <AboutUs />
+      <OurClass />
 
       <MoveToTop />
     </>
---
+++ src/pages/Our-Class/data.js
@@ -0,0 +1,134 @@
+const uiuxDesignCourses = [
+  {
+    id: 0,
+    imageUrl: "/assets/images/course-1.jpg",
+    title: "Introduction to Design System in Figma",
+    lessonCount: 16,
+    weekCount: 16,
+    price: 350,
+    rating: "4.9",
+  },
+  {
+    id: 1,
+    imageUrl: "/assets/images/course-2.jpg",
+    title: "Introduction to Design Thinking Framework",
+    lessonCount: 17,
+    weekCount: 16,
+    price: 360,
+    rating: "4.7",
+  },
+  {
+    id: 2,
+    imageUrl: "/assets/images/course-3.jpg",
+    title: "Design Principle and Fundamentals for UI Designer",
+    lessonCount: 15,
+    weekCount: 16,
+    price: 340,
+    rating: "4.8",
+  },
+  {
+    id: 3,
+    imageUrl: "/assets/images/course-4.jpg",
+    title: "Designing Responsive Dashboard for Fintech in Figma",
+    lessonCount: 19,
+    weekCount: 16,
+    price: 380,
+    rating: "5.0",
+  },
+];
+
+const webDevCourses = [
+  {
+    id: 0,
+    imageUrl: "/assets/images/course-3.jpg",
+    title: "100 Days Of Code - 2024 Web Development Bootcamp",
+    lessonCount: 18,
+    weekCount: 12,
+    price: 250,
+    rating: "4.8",
+  },
+  {
+    id: 1,
+    imageUrl: "/assets/images/course-4.jpg",
+    title: "The Ultimate 2024 Fullstack Web Development Bootcamp",
+    lessonCount: 14,
+    weekCount: 18,
+    price: 320,
+    rating: "4.9",
+  },
+  {
+    id: 2,
+    imageUrl: "/assets/images/course-1.jpg",
+    title: "Learn Complete Front-End Web Development Course",
+    lessonCount: 16,
+    weekCount: 20,
+    price: 140,
+    rating: "5.0",
+  },
+  {
+    id: 3,
+    imageUrl: "/assets/images/course-2.jpg",
+    title: "Web Development | HTML | CSS | JavaScript | jQuery | NodeJS",
+    lessonCount: 16,
+    weekCount: 19,
+    price: 280,
+    rating: "4.1",
+  },
+];
+
+const dataScienceCourses = [
+  {
+    id: 0,
+    imageUrl: "/assets/images/course-2.jpg",
+    title: "Introduction to Data Science",
+    lessonCount: 16,
+    weekCount: 16,
+    price: 350,
+    rating: "4.9",
+  },
+  {
+    id: 1,
+    imageUrl: "/assets/images/course-3.jpg",
+    title: "100 Days of Code: The Complete Python Pro Bootcamp",
+    lessonCount: 17,
+    weekCount: 16,
+    price: 360,
+    rating: "4.7",
+  },
+  {
+    id: 2,
+    imageUrl: "/assets/images/course-4.jpg",
+    title: "Complete A.I. & Machine Learning, Data Science Bootcamp",
+    lessonCount: 15,
+    weekCount: 16,
+    price: 340,
+    rating: "4.8",
+  },
+  {
+    id: 3,
+    imageUrl: "/assets/images/course-1.jpg",
+    title: "Python for Data Science and Machine Learning Bootcamp",
+    lessonCount: 19,
+    weekCount: 16,
+    price: 380,
+    rating: "5.0",
+  },
+];
+
+export const classes = [
+  {
+    id: 0,
+    class: "UI/UX Design",
+    data: uiuxDesignCourses,
+  },
+  {
+    id: 1,
+    class: "Web Development",
+    data: webDevCourses,
+  },
+  {
+    id: 2,
+    class: "Data Science",
+    data: dataScienceCourses,
+  },
+];
